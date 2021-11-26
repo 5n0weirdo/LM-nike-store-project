1a126b1f0f8bc506a47be176a07343a539b3103d
5n0weirdo <snoweirdo@protonmail.com>
2021-11-26 11:22:20 +0200

    Added dummy data

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,40 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { COLORS } from "../constants";
+import { COLORS, images, icons, FONTS, SIZES } from "../constants";
 
-const index = () => {
+const Home = () => {
   // Dummy Data
 
-  const [trending, setTrending] = React.useState([]);
+  const [trending, setTrending] = React.useState([
+    {
+      id: 0,
+      name: "Nike Air Zoom Pegasus 36",
+      img: images.nikePegasus36,
+      bgColor: "#BF012C",
+      type: "RUNNING",
+      price: "$186",
+      sizes: [6, 7, 8, 9, 10],
+    },
+    {
+      id: 1,
+      name: "Nike Metcon 5",
+      img: images.nikeMetcon5Black,
+      bgColor: "#D39C67",
+      type: "TRAINING",
+      price: "$135",
+      sizes: [6, 7, 8, 9, 10, 11, 12],
+    },
+    {
+      id: 2,
+      name: "Nike Air Zoom Kobe 1 Proto",
+      img: images.nikeZoomKobe1Proto,
+      bgColor: "#7052A0",
+      type: "BASKETBALL",
+      price: "$199",
+      sizes: [6, 7, 8, 9],
+    },
+  ]);
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
@@ -17,4 +46,4 @@ const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: COLORS.white },
 });
 
-export default index;
+export default Home;
