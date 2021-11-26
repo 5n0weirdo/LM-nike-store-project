e3d84f96c3590d768e2868132cf6b2015813d70c
5n0weirdo <snoweirdo@protonmail.com>
2021-11-26 10:42:08 +0200

    Shoe Selector added

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Image, TouchableOpacity } from "react-native";
+
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 // Screens
 import { Home } from "./screens";
+
+import { icons, COLORS, FONTS, SIZES } from "./constants";
 
 const theme = {
   ...DefaultTheme,
@@ -18,8 +22,30 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator initialRoutName={""}>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator initialRoutName={"Home"}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Shoe Selector",
+            headerTintColor: COLORS.lightGray,
+            headerTitleStyle: {
+              ...FONTS.navTitle,
+            },
+            headerLeft: ({ onPress }) => (
+              <TouchableOpacity
+                style={{ marginLeft: SIZES.padding }}
+                onPress={onPress}
+              >
+                <Image
+                  source={icons.menu}
+                  resizeMode="contain"
+                  style={{ width: 25, height: 25 }}
+                ></Image>
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
