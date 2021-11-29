a9df537ed10051301a3e48a747ff004a73b7013f
5n0weirdo <snoweirdo@protonmail.com>
2021-11-29 17:07:32 +0200

    Final

--- App.js
+++ App.js
@@ -1,71 +1,104 @@
-import React from "react";
-import { Image, TouchableOpacity } from "react-native";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
+import React from 'react';
+import {
+    Image,
+    TouchableOpacity
+} from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-// Screens
-import { Home } from "./screens";
+// screens
+import { Home } from "./screens/";
 
-import { icons, COLORS, FONTS, SIZES } from "./constants";
+import { images, icons, COLORS, FONTS, SIZES } from './constants';
+import { useFonts } from 'expo-font';
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    border: "transparent",
-  },
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
 };
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator initialRoutName={"Home"}>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: "Shoe Selector",
-            headerTintColor: COLORS.lightGray,
-            headerTitleStyle: {
-              ...FONTS.navTitle,
-            },
-            headerLeft: ({ onPress }) => (
-              <TouchableOpacity
-                style={{ marginLeft: SIZES.padding }}
-                onPress={onPress}
-              >
-                <Image
-                  source={icons.menu}
-                  resizeMode="contain"
-                  style={{ width: 25, height: 25 }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-            headerRight: () => (
-              <TouchableOpacity
-                style={{ marginRight: SIZES.padding }}
-                onPress={() => console.log("Pressed")}
-              >
-                <Image
-                  source={icons.search}
-                  resizeMode="contain"
-                  style={{
-                    width: 30,
-                    height: 30,
-                  }}
+
+    const [loaded] = useFonts({
+        "CarmenSans-Regular" : require('./assets/fonts/CarmenSans-Regular.otf'),
+        "CarmenSans-SemiBold" : require('./assets/fonts/CarmenSans-SemiBold.otf'),
+        "CarmenSans-Thin" : require('./assets/fonts/CarmenSans-Thin.otf'),
+        "CocoGothic-Bold" : require('./assets/fonts/CocoGothic-Bold.ttf'),
+        "CocoGothic": require('./assets/fonts/CocoGothic.ttf'),
+    })
+
+    if(!loaded){
+        return null;
+    }
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        title: 'SHOE SELECTOR',
+                        headerStyle: {
+                            //backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: COLORS.lightGray,
+                        headerTitleStyle: {
+                            ...FONTS.navTitle
+                        },
+                        headerLeft: ({ onPress }) => (
+                            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.menu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.search}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 30,
+                                        height: 30,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
                 />
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
+
 export default () => {
-  return <App />;
+    return <App />;
 };
